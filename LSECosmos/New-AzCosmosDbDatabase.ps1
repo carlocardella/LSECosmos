e7012ad15e68355a7e8a05cb4476ef5d0e3e4125
Carlo Cardella <carloc@microsoft.com>
2020-06-14 21:56:44 -0700

    but fixes

--- LSECosmos/New-AzCosmosDbDatabase.ps1
+++ LSECosmos/New-AzCosmosDbDatabase.ps1
@@ -1,41 +1,55 @@
-[CmdletBinding()]
-param (
-    [parameter(Mandatory, ValueFromPipelineByPropertyName)]
-    [string]$AccountNAme,
+function New-AzCosmosDbDatabase {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string]$AccountName,
     
-    [parameter(Mandatory, ValueFromPipelineByPropertyName)]
-    [string]$ResourceGroupName,
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [string]$ResourceGroupName,
 
-    [parameter(Mandatory, ValueFromPipelineByPropertyName)]
-    [Alias('Name')]
-    [string]$DatabaseName
-)
+        [parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [Alias('Name')]
+        [string]$DatabaseName,
+
+        [parameter(ValueFromPipelineByPropertyName)]
+        [string[]]$Locations,
+
+        [parameter()]
+        [switch]$EnableMultipleWriteLocations
+    )
 
 
-# Microsoft.DocumentDB/databaseAccounts/apis/databases/write
-# https://docs.microsoft.com/en-us/azure/cosmos-db/scripts/powershell/sql/ps-sql-create?toc=%2fpowershell%2fmodule%2ftoc.json
+    # Microsoft.DocumentDB/databaseAccounts/apis/databases/write
+    # https://docs.microsoft.com/en-us/azure/cosmos-db/scripts/powershell/sql/ps-sql-create?toc=%2fpowershell%2fmodule%2ftoc.json
 
-$location = Get-AzResourceGroup -Name $ResourceGroupName | Select-Object -ExpandProperty 'Location'
+    $location = Get-AzResourceGroup -Name $ResourceGroupName | Select-Object -ExpandProperty 'Location'
 
-$locations = @(
-    @{ "locationName" = "West US"; "failoverPriority" = 0 },
-    @{ "locationName" = "East US"; "failoverPriority" = 1 }
-)
+    $consistencyPolicy = @{
+        "defaultConsistencyLevel" = "BoundedStaleness";
+        "maxIntervalInSeconds"    = 300;
+        "maxStalenessPrefix"      = 100000
+    }
 
-$consistencyPolicy = @{
-    "defaultConsistencyLevel" = "BoundedStaleness";
-    "maxIntervalInSeconds"    = 300;
-    "maxStalenessPrefix"      = 100000
+    $CosmosDBProperties = @{
+        "databaseAccountOfferType" = "Standard";
+        "consistencyPolicy"        = $consistencyPolicy;
+    }
+    if ([string]::IsNullOrWhiteSpace($Locations)) {
+        $priority = 0
+        $Locations | ForEach-Object {
+            $CosmosDBProperties.locations += @{
+                'locationName'     = $_;
+                'failoverPriority' = $priority
+            }
+            $priority += 1
+        }
+    }
+    if ($EnableMultipleWriteLocations) {
+        $CosmosDBProperties.enalbeMultipleWriteLocations = 'true'
+    }
+
+    New-AzResource -ResourceType "Microsoft.DocumentDb/databaseAccounts" `
+        -ApiVersion "2015-04-08" -ResourceGroupName $ResourceGroupName -Location $location `
+        -Name $AccountName -PropertyObject $CosmosDBProperties
+
 }
-
-$CosmosDBProperties = @{
-    "databaseAccountOfferType"     = "Standard";
-    "locations"                    = $locations;
-    "consistencyPolicy"            = $consistencyPolicy;
-    "enableMultipleWriteLocations" = "true"
-}
-
-New-AzResource -ResourceType "Microsoft.DocumentDb/databaseAccounts" `
-    -ApiVersion "2015-04-08" -ResourceGroupName $ResourceGroupName -Location $location `
-    -Name $AccountNAme -PropertyObject $CosmosDBProperties
-
